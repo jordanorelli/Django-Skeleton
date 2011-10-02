7030a8cdee179dad9db2556e0fb75fcac4e9a906
Jordan Orelli <jordanorelli@gmail.com>
2011-10-02 15:46:38 -0400

    disable crawling by default

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -1,18 +1,19 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.views.generic import TemplateView
+from django.http import HttpResponse
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
-    # url(r'^proj/', include('proj.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^robots.txt$', lambda x: HttpResponse("User-Agent: *\nDisallow: /",
+                                                mimetype="text/plain")),
 )
